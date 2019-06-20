4f8a7d7cbe68418dde458dbb6e198dd25adf6b04
shanreed <srtech5set@gmail.com>
2019-06-19 19:11:39 -0700

    login with correct credentials working

--- requests/auth/auth-router.js
+++ requests/auth/auth-router.js
@@ -23,14 +23,21 @@ router.post('/login', (req, res) => {
   Users.findBy({ username })
     .first()
     .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
         res.status(200).json({
           message: `Welcome back ${user.username}!`
         });
-    })
+  } else{
+    res.status(401).json({ message: 'Invalid Credentials' });
+  }
+})
     .catch(error => {
       res.status(500).json(error);
     });
 });
+
+
+
 
 
 
