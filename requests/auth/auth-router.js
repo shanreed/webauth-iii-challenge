4105985a6871b8d8e11d6ea4e1f4789d0ca65474
shanreed <srtech5set@gmail.com>
2019-06-19 18:54:12 -0700

    added bcryptjs and hash for passwords, working

--- requests/auth/auth-router.js
+++ requests/auth/auth-router.js
@@ -1,10 +1,13 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
 
 const Users = require('../users/users-model.js');
 
 
 router.post('/register', (req, res) => {
   let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 8);
+  user.password = hash;
   Users.add(user)
     .then(newlogininfo => {
       res.status(201).json({message: `Welcome ${user.username}, below is your login information, you will need it next time you login!`, newlogininfo});
